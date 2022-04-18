9826df375b9f251e88ff1bf79f5fe40f416292a3
PowersHK <99101047+PowersHK@users.noreply.github.com>
2022-04-18 12:42:52 -0400

    Create music.sql

---
+++ music.sql
@@ -0,0 +1,18 @@
+CREATE TABLE artists (
+  id INTEGER PRIMARY KEY,
+  artist TEXT,
+);
+
+CREATE TABLE albums (
+  id INTEGER PRIMARY KEY,
+  artist_name TEXT,
+  album_name TEXT,
+);
+
+CREATE TABLE songs (
+  id INTEGER PRIMARY KEY,
+  album_name TEXT,
+  song_name TEXT,
+  track_number INTEGER,
+  track_length INTEGER
+);
